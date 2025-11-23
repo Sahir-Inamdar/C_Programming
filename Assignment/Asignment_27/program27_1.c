1ba0c8bb89f7835824c8aa9b2fc51dd850e19c0e
Sahir-Inamdar <sahirinamdar.code@gmail.com>
2025-11-23 22:33:03 +0530

    Add files via upload
    Solutions for Assignment-27(Q1-Q5)

---
+++ Assignment/Asignment_27/program27_1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name: Pattern
+// Description: displays pattern 
+// Input: int
+// Output : void
+// Author : Sahir Saiduddin Inamdar
+// Date : 20/11/25
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j =0;
+
+    for(i = 1 ; i <= iRow; i++ )
+    {
+        for(j = 1 ; j <= iCol; j++ )
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+
+} 
