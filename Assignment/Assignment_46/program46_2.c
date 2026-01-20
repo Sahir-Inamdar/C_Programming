bee995b8ad578358e155554caa8b68b7bc2d916b
Sahir-Inamdar <sahirinamdar.code@gmail.com>
2026-01-20 17:58:34 +0530

    Add files via upload
    solutions for Assignment_46(Q1-Q5)

---
+++ Assignment/Assignment_46/program46_2.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+struct node
+{
+ int Data;
+ struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head , int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Next = NULL;
+    newn->Data = no;
+    if (*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn -> Next = *Head;
+        *Head = newn;
+    }
+}
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d |->",Head->Data);
+        Head = Head->Next;
+    }
+    printf("NULL \n");
+}
+
+
+void DisplayLess(PNODE Head, int X)
+{
+    printf("Number smaller then %d are :\n",X);
+    
+    while(Head != NULL)
+    {
+        if(Head->Data < X)
+        {
+            printf("%d\n",Head->Data);
+        }
+        Head = Head->Next;
+        
+    }
+}
+
+int Count(PNODE Head)
+{
+    int iCount = 0;
+
+    while(Head != NULL)
+    {
+        iCount++;
+        Head = Head->Next;
+    }
+    return iCount;
+}
+
+
+int main()
+{
+    int iRet = 0;
+    PNODE First = NULL;
+    InsertFirst(&First, 640);
+    InsertFirst(&First, 25);
+    InsertFirst(&First, 23);
+    InsertFirst(&First, 230);
+    InsertFirst(&First, 110);
+
+    Display(First);
+
+    iRet = Count(First);
+
+    printf("number of elements in node are : %d\n",iRet);
+    
+    DisplayLess(First,220);
+        
+    return 0;
+} 
---
+++ Assignment/Assignment_46/program46_3.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+struct node
+{
+ int Data;
+ struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head , int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Next = NULL;
+    newn->Data = no;
+    if (*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn -> Next = *Head;
+        *Head = newn;
+    }
+}
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d |->",Head->Data);
+        Head = Head->Next;
+    }
+    printf("NULL \n");
+}
+
+
+bool IsEmpty(PNODE Head)
+{
+    bool bFlag = false;
+
+    if(Head == NULL)
+    {
+        bFlag = true;   
+    }
+
+    return bFlag;
+}
+
+int Count(PNODE Head)
+{
+    int iCount = 0;
+
+    while(Head != NULL)
+    {
+        iCount++;
+        Head = Head->Next;
+    }
+    return iCount;
+}
+
+
+int main()
+{
+    int iRet = 0;
+    bool fRet = false;
+    PNODE First = NULL;
+    InsertFirst(&First, 640);
+    InsertFirst(&First, 25);
+    InsertFirst(&First, 23);
+    InsertFirst(&First, 230);
+    InsertFirst(&First, 110);
+
+    Display(First);
+
+    iRet = Count(First);
+
+    printf("number of elements in node are : %d\n",iRet);
+    
+    fRet = IsEmpty(First);
+
+    if(fRet == false)
+    {
+        printf("List has least have one node\n");
+        printf("number of elements in node are : %d\n",iRet);
+    }
+    else
+    {
+        printf("List is Empty");
+    }
+        
+    return 0;
+} 
