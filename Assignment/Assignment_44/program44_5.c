0dcb46a1025be3fe1b79ae9c1522ed40ac3dfb4c
Sahir-Inamdar <sahirinamdar.code@gmail.com>
2026-01-20 17:54:49 +0530

    Add files via upload
    solutions for Assignment_44(Q1-Q5)

---
+++ Assignment/Assignment_44/program44_5.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+struct node
+{
+ int Data;
+ struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head , int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Next = NULL;
+    newn->Data = no;
+    if (*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn -> Next = *Head;
+        *Head = newn;
+    }
+}
+void Display(PNODE Head)
+{
+    while(Head != NULL)
+    {
+        printf("| %d |->",Head->Data);
+        Head = Head->Next;
+    }
+    printf("NULL \n");
+}
+
+
+void DisplayEven(PNODE Head)
+{
+    printf("Even numbers are :\n");
+
+    while(Head != NULL)
+    {
+        if(Head->Data % 2 == 0)
+        {
+            printf("%d\t",Head->Data);
+        }
+        Head = Head->Next;
+    }
+}
+
+int Count(PNODE Head)
+{
+    int iCount = 0;
+
+    while(Head != NULL)
+    {
+        iCount++;
+        Head = Head->Next;
+    }
+    return iCount;
+}
+
+
+int main()
+{
+    int iRet = 0;
+    PNODE First = NULL;
+    InsertFirst(&First, 640);
+    InsertFirst(&First, 240);
+    InsertFirst(&First, 20);
+    InsertFirst(&First, 230);
+    InsertFirst(&First, 110);
+
+    Display(First);
+
+    iRet = Count(First);
+
+    printf("number of elements in node are : %d\n",iRet);
+    
+    DisplayEven(First);    
+    
+    return 0;
+} 
